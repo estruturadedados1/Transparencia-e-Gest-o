8d4e533cc0651d5e3652540c73a41fdfdec888b0
estruturadedados1 <40615641+estruturadedados1@users.noreply.github.com>
2018-07-06 12:24:20 -0300

    Add files via upload
    Código alterado para receber como parâmetro arquivo contendo lista de arquivos a serem analisados e uma série de mudanças estruturais.

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct listaArquivos {
+    char *nomeArquivo;
+    struct listaArquivos *proximoArquivo;
+} listaArquivos;
+typedef struct listaArquivos* Arquivo;
 
 typedef unsigned long int Codigo;
+
+typedef struct registro {
+    char * arquivo;
+    ssize_t linha;
+    struct registro * proximoRegistro;
+} registro;
+typedef registro * Registro;
 
 typedef struct funcionario {
     Codigo codigo;
     double rendimentos;
+    int numRegistros;
+    Registro pilhaRegistros;
 } funcionario;
 typedef funcionario * Funcionario;
 
@@ -69,7 +88,7 @@ void imprimirArvoreCrescente(No arvore) {
     }
     imprimirArvoreCrescente(arvore->esquerdo);
     if (arvore->funcionario != NULL) {
-        printf("Codigo: %zu, Rendimentos %.2f\n", arvore->funcionario->codigo, arvore->funcionario->rendimentos);
+        printf("Codigo: %zu, Registros: %d, Rendimentos: %.2f\n", arvore->funcionario->codigo, arvore->funcionario->numRegistros, arvore->funcionario->rendimentos);
     }
     imprimirArvoreCrescente(arvore->direito);
 }
@@ -85,88 +104,210 @@ void imprimirArvoreDecrescente(No arvore) {
     imprimirArvoreDecrescente(arvore->esquerdo);
 }
 
-int main(int argc, char** argv) {
+Registro ultimoRegistro(Registro reg) {
+    if (reg) {
+        while (reg->proximoRegistro) {
+            reg = reg->proximoRegistro;
+        }
+    }
+    return reg;
+}
+
+No carregarRegistros(Arquivo listaArquivos, size_t limite) {
 
     FILE *arquivo;
+    No arvore = NULL;
+    char *linha = NULL, *token = NULL, *separador = "\t";
+    int numArquivo = 0;
+
+    while (listaArquivos) {
+
+        numArquivo += 1;
+
+        char *nomeArquivo = listaArquivos->nomeArquivo;
+
+        ssize_t caracteres = 0;
+        size_t len = 0, numLinha = 0;
+
+        arquivo = fopen(nomeArquivo, "r");
+        if (arquivo) {
+            printf("\nProcessando o arquivo %s...", nomeArquivo);
+        } else {
+            printf("\nO arquivo %s nao pode ser aberto!\n\n", nomeArquivo);
+            return NULL;
+        }
+
+        while (((caracteres = getline(&linha, &len, arquivo)) != -1) && (numLinha < limite)) {
+
+            numLinha += 1;
+
+            //Remove o cabecalho
+
+            if (numLinha > 1) {
+
+                if ((numLinha % 500) == 0) {
+                    printf("%zu...", numLinha);
+                }
+
+                token = strtok(linha, separador);
+
+                Codigo codigo;
+                double rendimentos = 0.0;
+                int num_campo = 0;
+
+                while (token != NULL) {
+
+                    num_campo += 1;
+
+                    //printf(" %s\n", token);
+                    switch (num_campo) {
+                        case 3:
+                            //printf("Codigo: %s\n", token);
+                            codigo = atol(token);
+                            break;
+                        case 10:
+                        case 14:
+                        case 16:
+                        case 22:
+                        case 32:
+                        case 34:
+                            //printf("Valor: %.2f\n", atof(token));
+                            rendimentos += atof(token);
+                            break;
+                    }
+
+                    token = strtok(NULL, separador);
+                }
+
+                //printf("Buscanco funcionario...\n");
+
+                Registro reg = (Registro) malloc(sizeof (registro));
+
+                if (reg) {
+                    reg->arquivo = nomeArquivo;
+                    reg->linha = numLinha;
+                    reg->proximoRegistro = NULL;
+                }
+
+                No no_funcionario = buscarNo(codigo, arvore);
+
+                if (no_funcionario == NULL) {
+                    no_funcionario = (No) malloc(sizeof (no));
+                    no_funcionario->funcionario = (Funcionario) malloc(sizeof (funcionario));
+                    no_funcionario->funcionario->codigo = codigo;
+                    no_funcionario->funcionario->rendimentos = rendimentos;
+                    no_funcionario->funcionario->numRegistros = 1;
+                    no_funcionario->funcionario->pilhaRegistros = reg;
+                    no_funcionario->direito = NULL;
+                    no_funcionario->esquerdo = NULL;
+                    inserirNo(no_funcionario, &arvore);
+                    //inserirNoPorRendimento(no_funcionario, &arvore);
+                } else {
+                    no_funcionario->funcionario->rendimentos += rendimentos;
+                    no_funcionario->funcionario->numRegistros += 1;
+                    ultimoRegistro(no_funcionario->funcionario->pilhaRegistros)->proximoRegistro = reg;
+                }
+
+                reg = NULL;
+                no_funcionario = NULL;
+
+            }
+
+        }
+
+        printf("[%zu]!", numLinha);
+
+        if (arquivo) {
+            fclose(arquivo);
+            arquivo = NULL;
+        }
+        arquivo = NULL;
+        if (linha) {
+            free(linha);
+            linha = NULL;
+        }
+
+        listaArquivos = listaArquivos->proximoArquivo;
+    }
+
+    return arvore;
+}
+
+Arquivo ultimoArquivo(Arquivo lista) {
+    if (lista) {
+        while (lista->proximoArquivo) {
+            lista = lista->proximoArquivo;
+        }
+    }
+    return lista;
+}
+
+int main(int argc, char** argv) {
+
+    Arquivo listaArquivos = NULL;
+    size_t limite = 10;
+
+    if (argc != 2) {
+        printf("\nInforme o arquivo contendo o caminho para os arquivos que serao analisados!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *arquivo = fopen(argv[1], "r");
+
+    if (arquivo == NULL) {
+        printf("\nErro ao abrir arquivo \'%s\'!\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
     char *linha = NULL;
     size_t len = 0;
-    ssize_t tamanhoDaLinha;
-    int limite = 0;
-    char *token;
-    char *separador = "\t";
-    No arvore = NULL;
+    ssize_t caracteres = 0;
 
-    arquivo = fopen("/tmp/20180531_Remuneracao.csv", "r");
-    if (arquivo == NULL)
-        exit(EXIT_FAILURE);
+    while (caracteres = getline(&linha, &len, arquivo) != -1) {
 
-    while (((tamanhoDaLinha = getline(&linha, &len, arquivo)) != -1) && (limite++ < 100000)) {
+        int ultimoCaractere = strlen(linha) - 1;
 
-        //printf("Tamanho da linha %zu :\n", tamanhoDaLinha);
-        //printf("%s", linha);
-
-        if ((limite % 500) == 0) {
-            printf("%d...", limite);
+        if (linha[ultimoCaractere] == '\n') {
+            linha[ultimoCaractere] = '\0';
         }
 
-        token = strtok(linha, separador);
+        Arquivo arq = (Arquivo) malloc(sizeof (listaArquivos));
+        char *str = (char*) malloc(sizeof (char)*(strlen(linha)));
+        strncpy(str, linha, strlen(linha));
+        arq->nomeArquivo = str;
+        arq->proximoArquivo = NULL;
 
-        Codigo codigo;
-        double rendimentos = 0.0;
+        Arquivo aux = ultimoArquivo(listaArquivos);
 
-        int num_campo = 1;
-        while (token != NULL) {
-            //printf(" %s\n", token);
-            if (limite > 1) {
-                switch (num_campo++) {
-                    case 3:
-                        //printf("Codigo: %s\n", token);
-                        codigo = atol(token);
-                        break;
-                    case 10:
-                    case 14:
-                    case 16:
-                    case 22:
-                    case 32:
-                    case 34:
-                        //printf("Valor: %.2f\n", atof(token));
-                        rendimentos += atof(token);
-                        break;
-                }
-            }
-            token = strtok(NULL, separador);
-        }
-
-        //printf("Buscanco funcionario...\n");
-
-        No no_funcionario = buscarNo(codigo, arvore);
-
-        if (no_funcionario == NULL) {
-            no_funcionario = (No) malloc(sizeof (no));
-            no_funcionario->funcionario = (Funcionario) malloc(sizeof (funcionario));
-            no_funcionario->funcionario->codigo = codigo;
-            no_funcionario->funcionario->rendimentos = rendimentos;
-            no_funcionario->direito = NULL;
-            no_funcionario->esquerdo = NULL;
-            inserirNo(no_funcionario, &arvore);
-            //inserirNoPorRendimento(no_funcionario, &arvore);
+        if (aux) {
+            aux->proximoArquivo = arq;
         } else {
-            no_funcionario->funcionario->rendimentos += rendimentos;
+            listaArquivos = arq;
         }
-
 
     }
 
-    printf("%d...\n", limite);
-    
-    //imprimirArvoreCrescente(arvore);
-    No arvoreOrdenadaPorRendimentos = NULL;
-    ordenarPorRendimento(arvore, &arvoreOrdenadaPorRendimentos);
-    imprimirArvoreDecrescente(arvoreOrdenadaPorRendimentos);
+    if (arquivo) {
+        free(arquivo);
+        arquivo = NULL;
+    }
 
-    fclose(arquivo);
-    if (linha)
-        free(linha);
+    No arvore = carregarRegistros(listaArquivos, limite);
+
+    if (arvore) {
+
+        printf("\n\nFuncionarios:\n\n");
+        imprimirArvoreCrescente(arvore);
+        No arvoreOrdenadaPorRendimentos = NULL;
+
+        printf("\n\nFuncionarios ordenados:\n\n");
+
+        ordenarPorRendimento(arvore, &arvoreOrdenadaPorRendimentos);
+        imprimirArvoreDecrescente(arvoreOrdenadaPorRendimentos);
+
+        printf("\n\nConcluido!\n\n");
+
+    }
 
     return (EXIT_SUCCESS);
 }
